55b1252dd81009bf85237a9235749b40e86fbf06
Keniran <naani.navis@gmail.com>
2021-03-28 11:55:12 -0400

    did some stuff

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,35 @@
 public class Employee implements Comparable {
 
+    protected int salary;
+    protected String name;
+    protected String department;
+    protected String title;
+
+    public Employee(int salary, String name, String department, String title) {
+        this.salary = salary;
+        this.name = name;
+        this.department = department;
+        this.title = title;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
 public class Manager extends Employee {
 
+    protected int bonus;
+    protected ArrayList reports;
+
+    public Manager(int salary, int bonus, String name, String department, String title, ArrayList reports, int tier) {
+        super(salary, name, department, title);
+        this.bonus = bonus;
+        this.reports = reports;
+    }
 }
