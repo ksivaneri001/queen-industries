d680999677dc90febbb022f8b80eee50eece9863
Keniran <naani.navis@gmail.com>
2021-03-29 14:01:48 -0400

    added reassigning log

--- src/Manager.java
+++ src/Manager.java
@@ -44,6 +44,29 @@ public class Manager extends Employee {
         }
     }
 
+    public void fire(Manager employee) throws Exception {
+        List<Employee> managerless = employee.getReports();
+        if (!employee.getDepartment().equals(this.getDepartment())) throw new Exception("ERROR: cannot fire an Employee who is not a direct or indirect report.");
+        else if (this.compareTo(employee) <= 0) throw new Exception("ERROR: cannot fire an Employee of an equal or greater tier.");
+        else {
+            this.reports.remove(employee);
+            System.out.println("LOG: existing Employee fired (" + employee.getName() + ", " + employee.getDepartment() + ", " + employee.getTitle() + ")");
+
+            String reassignedLog = "";
+            boolean reassigned = false;
+            for (int i = 0; i < this.getReports().size(); i++) {
+                if (this.reports.get(i).getDepartment().equals(employee.getDepartment())) {
+                    for (Employee employee2 : managerless) {
+                        reassigned = true;
+                        this.reports.add(employee2);
+                        reassignedLog += employee2.getName() + ", " + employee2.getDepartment() + ", " + employee2.getTitle() + ", ";
+                    }
+                }
+            }
+            if (reassigned) System.out.println("LOG: reports re-assigned [" + reassignedLog.substring(0, reassignedLog.length() - 2) + "]");
+        }
+    }
+
     public void adjustSalary(int amount, Employee employee) throws Exception {
         if (this.compareTo(employee) <= 0) throw new Exception("ERROR: cannot supervise an Employee of an equal or greater tier.");
         else employee.salary += amount;
